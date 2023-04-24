5132726c97d23192e886148d96aeb99b30300e25
Lucca Giordano <giordanolucca871@gmail.com>
2023-04-24 19:21:41 -0300

    arquivos do godot

--- Iniciar.cs
+++ Iniciar.cs
@@ -1,18 +1,15 @@
 using Godot;
 using System;
 
-public partial class JogoPrincipal : Node2D
+public partial class Iniciar : Button
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var mouse = ResourceLoader.Load("res://assets/Cursor.png");
-		Input.SetCustomMouseCursor(mouse);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GB.Print(GetMousePosition());
 	}
 }
--- script/JogoPrincipal.cs
+++ script/JogoPrincipal.cs
@@ -1,26 +1,17 @@
 using Godot;
 using System;
 
-public partial class ButtonCred : Button
+public partial class JogoPrincipal : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		var mouse = ResourceLoader.Load("res://assets/Cursor.png");
+		Input.SetCustomMouseCursor(mouse);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	
-	private void _on_pressed()
-	{
-		GetTree().ChangeSceneToFile("res://Tela de Créditos.tscn");
-	}
-
-	private void _on_button_pressed()
-	{
-		GetTree().ChangeSceneToFile("res://JogoPrincipal.tscn");
-	}
-
 }
