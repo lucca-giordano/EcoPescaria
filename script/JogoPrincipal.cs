dab0651f4d1a56695614e08942991b7c1a1c3812
Lucca Giordano <giordanolucca871@gmail.com>
2023-05-04 23:37:50 -0300

    updates

--- script/JogoPrincipal.cs
+++ script/JogoPrincipal.cs
@@ -12,21 +12,40 @@ public partial class JogoPrincipal : Node2D
 	static Texture2D primaveraSprite = (Texture2D)GD.Load("res://assets/estacoes/Primavera.png");
 
 	private int seasonIndex = 0;
-	Texture2D atual;
+	public Node2D atual;
 	public Texture2D[] estacoes = { veraoSprite, outonoSprite, invernoSprite, primaveraSprite };
+
+	public static Node2D verao, outono, inverno, primavera;
+	public Node2D[] estacoesNode = { verao, outono, inverno, primavera };
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var mouse = ResourceLoader.Load("res://assets/Cursor.png");
 		Input.SetCustomMouseCursor(mouse);
+
+		verao = GetNode<Node2D>("Verao");
+		outono = GetNode<Node2D>("Outono");
+		inverno = GetNode<Node2D>("Inverno");
+		primavera = GetNode<Node2D>("Primavera");
+
+		// torna a visibilidade de todas por padrão falsa
+		verao.Visible = false;
+		outono.Visible = false;
+		inverno.Visible = false;
+		primavera.Visible = false;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		DrawTexture(estacoes[seasonIndex], new Vector2(100, 100));
-		GD.Print("O sprite foi impresso");
+		// atualiza a estação atual, deixando uma visivel depois a outra
+		atual = estacoesNode[seasonIndex];
+		atual.Visible = true;
+
+
+
 	}
 	
 	private void _on_sprite_estacao_draw()
