6a5933f9920c2ab6430310517eadef983cfb802b
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-05-10 11:10:08 -0300

    player and tiles

--- scripts/SpawnerDireita.cs
+++ scripts/SpawnerDireita.cs
@@ -3,23 +3,13 @@ using System;
 
 public partial class SpawnerDireita : CharacterBody2D
 {
-	public Vector2 spd = Vector2(100, 0);
-	public Vector2 posicaoInicial;
-
 	public override void _Ready()
 	{
-		posicaoInicial = Position;
+		
 	}
 	
 	public override void _Process(double delta)
 	{
-// Acesse a posição do objeto
-		Vector2 posicao = Position;
-
-		// Atualize a posição do objeto para a direita
-		posicao.X += spd;
-
-		// Defina a nova posição do objeto
-		Position = posicao;
+		
 	}
 }
