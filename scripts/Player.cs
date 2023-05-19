6b157d9bdb5955bd9b07fca31bb9c7494c92f264
Lucca Giordano <122946858+lucca-giordano@users.noreply.github.com>
2023-05-19 10:00:54 -0300

    decorations

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -14,6 +14,8 @@ public partial class Player : CharacterBody2D
 	{
 		AnimatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		AnimatedSprite.Play("idle");
+		// fazer a vara não aparecer ao iniciar o jogo
+		GetNode<Sprite2D>("FishingRod").Visible = false;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -22,20 +24,26 @@ public partial class Player : CharacterBody2D
 
 		// Add the gravity.
 		if (!IsOnFloor())
+		{
 			velocity.Y += gravity * (float)delta;
+			AnimatedSprite.Play("jump");
+		}
 		
 		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
 			velocity.Y = JumpVelocity;
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("A", "D", "W", "S");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
 			AnimatedSprite.Play("run");
 		}
+		
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
@@ -44,9 +52,14 @@ public partial class Player : CharacterBody2D
 
 		//inverter o sprite de acordo com a direção
 		if (direction.X < 0)
+		{
 			AnimatedSprite.FlipH = true;
+		}
+		
 		else if (direction.X > 0)
+		{
 			AnimatedSprite.FlipH = false;
+		}
 
 		Velocity = velocity;
 		MoveAndSlide();
@@ -60,6 +73,11 @@ public partial class Player : CharacterBody2D
 
 
 	private void _on_area_de_pesca_body_exited(Node2D body)
+	{
+		GetNode<Sprite2D>("FishingRod").Visible = false;
+	}
+	
+	private void _on_fishing_rod_ready()
 	{
 		GetNode<Sprite2D>("FishingRod").Visible = false;
 	}
